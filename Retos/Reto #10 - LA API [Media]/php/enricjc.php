fc85b4aa6f1a96c0fb8ef2e125edf5b33587eb92
Enric Jódar <enricjc@gmail.com>
2023-09-01 16:27:08 +0200

    Reto 10 - PHP (PR)

---
+++ Retos/Reto #10 - LA API [Media]/php/enricjc.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+class Api
+{
+
+    public function call(string $url)
+    {
+
+        if ($url != 'https://cat-fact.herokuapp.com/facts') {
+            throw new \ErrorException('Endpoint URL is not correct');
+        }
+
+        try {
+            $curl = curl_init();
+
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($curl);
+            $facts = json_decode($response, true);
+        } catch (Exception $e) {
+            throw new \ErrorException($e);
+        }
+
+        foreach ($facts as $key => $fact) {
+            if (isset($fact['text'])) {
+                echo $fact['text'] . PHP_EOL;
+            }
+        }
+    }
+}
